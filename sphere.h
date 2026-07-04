729800df8c8048a52021765a4767de4c28ea41e0
SuriTian <suritian9@gmail.com>
2026-07-04 00:40:44 -0400

    make all intervals abstract into an intervals class

--- sphere.h
+++ sphere.h
@@ -7,7 +7,7 @@ class sphere : public hittable {
     public:
         sphere(const point3& center, double radius) : center(center), radius(fmax(0, radius)) {}
 
-        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = center - r.origin();
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
@@ -20,9 +20,9 @@ class sphere : public hittable {
 
             // find nearest root that lies in acceptable range 
             auto root = (h - sqrtdis) / a; // simplified form of [-b - sqrt(b^2 - 4ac)] / 2a
-            if (root <= ray_tmin || root >= ray_tmax) { // check - root
+            if (!ray_t.surrounds(root)) { // check - root
                 root = (h + sqrtdis) / a; // check + root
-                if (root <= ray_tmin || root >= ray_tmax) return false; 
+                if (!ray_t.surrounds(root)) return false; 
             }
 
             rec.t = root; // took the root that was in bounds 
