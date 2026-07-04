b36f0feda6cfd333d04618d972aa6c538f1a1483
SuriTian <suritian9@gmail.com>
2026-07-04 02:09:01 -0400

    smoothness++

--- camera.h
+++ camera.h
@@ -9,6 +9,7 @@ class camera {
     public:
         double aspect_ratio = 1.0; // width / height
         int image_width = 100; // pixel count
+        int samples_per_pixel = 10; // count of random samples per pixel for anti-aliasing
 
         void render(const hittable& world) {
             initialize(); 
@@ -18,25 +19,13 @@ class camera {
 
             for (int j = 0; j < image_height; j++) { // stacks the rows top to bottom
                 clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush; // progress indicter
-                for (int i = 0; i < image_width; i++) { // row
-                    // auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0); 
-                    
-                    auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                    auto ray_direction = pixel_center - camera_center; 
-                    ray r(camera_center, ray_direction);
-                    
-                    color pixel_color = ray_color(r, world);
-                    write_color(cout, pixel_color); 
-
-                    // auto r = double(i) / (image_width - 1);
-                    // auto g = double(j) / (image_height - 1); 
-                    // auto b = 0.0; 
-
-                    // int ir = int(255.999 * r);
-                    // int ig = int(255.999 * g);
-                    // int ib = int(255.999 * b);
-
-                    // cout << ir << ' ' << ig << ' ' << ib << '\n'; 
+                for (int i = 0; i < image_width; i++) { // row                    
+                    color pixel_color(0,0,0);
+                    for (int sample = 0; sample < samples_per_pixel; sample++) {
+                        ray r = get_ray(i, j);
+                        pixel_color += ray_color(r, world);
+                    }
+                    write_color(cout, pixel_samples_scale * pixel_color);
                 }
             }
 
@@ -46,6 +35,7 @@ class camera {
 
     private:
         int image_height;
+        double pixel_samples_scale;
         point3 camera_center;
         point3 pixel00_loc; // location of pixel 0, 0
         vec3 pixel_delta_u; // offset to pixel to right
@@ -55,6 +45,8 @@ class camera {
         void initialize() {
             image_height = int(image_width / aspect_ratio); // respect the aspect ratio
             image_height = (image_height < 1) ? 1 : image_height; // for some reason cannot have less than 1, possibly because it would not render
+
+            pixel_samples_scale = 1.0 / samples_per_pixel;
 
             camera_center = point3(0, 0, 0);
 
@@ -67,15 +59,30 @@ class camera {
             auto viewport_v = vec3(0, -viewport_height, 0);
 
              // Calculate the horizontal and vertical delta vectors form pixel to pixel 
-            auto pixel_delta_u = viewport_u / image_width;
-            auto pixel_delta_v = viewport_v / image_height;
+            pixel_delta_u = viewport_u / image_width;
+            pixel_delta_v = viewport_v / image_height;
 
             // Calculate location of upper left pixel
             auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-            auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v); 
+            pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v); 
+            pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v); 
 
         }
         
+        ray get_ray(int i, int j) const {
+            auto offset = sample_square();
+            auto pixel_sample = pixel00_loc + (i + offset.x()) * pixel_delta_u + (j + offset.y()) * pixel_delta_v;
+
+            auto ray_origin = camera_center;
+            auto ray_direction = pixel_sample - ray_origin;
+
+            return ray(ray_origin, ray_direction);
+        }
+
+        // next steps: try non square pixels
+        vec3 sample_square() const {
+            return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+        }
 
         color ray_color(const ray& r, const hittable& world) const {
             hit_record rec;
--- rtweekend.h
+++ rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -16,6 +17,16 @@ const double pi = 3.1415926535897932385;
 // utility functions
 inline double degrees_to_radians(double degree) {
     return degree * pi / 180.0;
+}
+
+inline double random_double() {
+    // real in [0, 1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // real in [min, max)
+    return min + (max - min) * random_double(); 
 }
 
 #include "color.h"
