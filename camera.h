6e35cd9285289d132ad9ff781298c1abd405c20c
SuriTian <suritian9@gmail.com>
2026-07-05 15:01:24 -0400

    metallic shine

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 using namespace std;
 
@@ -90,9 +91,15 @@ class camera {
             if (depth <= 0) return color(0,0,0);
 
             hit_record rec;
+
             if (world.hit(r, interval(0.001, infinity), rec)) {
-                vec3 direction = rec.normal + random_unit_vector();
-                return 0.1 * ray_color(ray(rec.p, direction), depth - 1, world); // adjust reflectance
+                ray scattered;
+                color attenuation;
+                if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+                    return attenuation * ray_color(scattered, depth - 1, world);
+                }
+
+                return color(0,0,0);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
