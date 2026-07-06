aa57a725842aa831dd29418e6581ce372d225c73
SuriTian <suritian9@gmail.com>
2026-07-05 20:04:58 -0400

    brush up refraction

--- material.h
+++ material.h
@@ -70,7 +70,7 @@ class dielectric : public material {
             bool cannot_refract = ri * sin_theta > 1.0;
             vec3 direction;
 
-            if (cannot_refract) {
+            if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
                 direction = reflect(unit_direction, rec.normal);
             } else {
                 direction = refract(unit_direction, rec.normal, ri);
@@ -82,6 +82,13 @@ class dielectric : public material {
 
     private:
         double refraction_index;
+
+        // schlicks approx. for reflectance
+        static double reflectance(double cosine, double ref_idx) {
+            auto r0 = (1 - ref_idx) / (1 + ref_idx);
+            r0 = r0 * r0;
+            return r0 + (1 - r0) * pow((1 - cosine), 5);
+        }
 };
 
 #endif
